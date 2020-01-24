d455b39c7c5950288e743ae96144ba1ac6e351fd
Zach White <zechnwhite@gmail.com>
2020-01-24 00:06:37 -0800

    removes extra /profile route

--- server.js
+++ server.js
@@ -163,26 +163,28 @@ app.post('/login', async (req, res) => {
     });
 });
 
-app.get('/profile', (req, res) => {
-  let decoded = jwt.verify(
-    req.headers['authorization'],
-    process.env.JWT_SECRET
-  );
+// app.get('/profile', (req, res) => {
+//   let decoded = jwt.verify(
+//     req.headers['authorization'],
+//     process.env.JWT_SECRET
+//   );
 
-  User.findOne({
-    _id: decoded._id
-  })
-  .then(user => {
-    if (user) {
-      res.json(user);
-    } else {
-      res.send('User does not exist.');
-    }
-  })
-  .catch(err => {
-    res.send('error: ' + err);
-  });
-});
+//   User.findOne({
+//     _id: decoded._id
+//   })
+//   .then(user => {
+//     if (user) {
+//       res.json(user);
+//     } else {
+//       res.send('User does not exist.');
+//     }
+//   })
+//   .catch(err => {
+//     res.send('error: ' + err);
+//   });
+// });
+
+// getting rid of code above to allow profile page refresh
 
 app.get('/test', (req, res) => {
   User.find({})
@@ -199,7 +201,7 @@ app.get('/test', (req, res) => {
 const PORT = process.env.PORT || 5000;
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static( 'client/build' ));
-    app.use('*', express.static('client/build')); // added as per https://stackoverflow.com/questions/45419943/express-react-router-internal-server-error-when-linked-directly
+    // app.use('*', express.static('client/build')); // added as per https://stackoverflow.com/questions/45419943/express-react-router-internal-server-error-when-linked-directly
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     })
